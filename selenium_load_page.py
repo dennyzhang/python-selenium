da601d28b2ada453fb5ffec7489d2faa6660e2eb
dennyzhang <denny@dennyzhang.com>
2017-09-06 11:57:47 -0500

    update type

--- selenium_load_page.py
+++ selenium_load_page.py
@@ -101,8 +101,7 @@ if __name__ == '__main__':
                         help="If page load takes too long, quit the test", type=int)
     parser.add_argument('--should_save_screenshot', required=False, dest='should_save_screenshot', \
                         action='store_true', default=True, \
-                        help="Once enabled, selenium will save the page as screenshot in the selenium server", \
-                        type=bool)
+                        help="Once enabled, selenium will save the page as screenshot in the selenium server")
     parser.add_argument('--screenshot_dir', required=False, default="/tmp/screenshot""", \
                         help="Where to save screenshots", type=str)
 
